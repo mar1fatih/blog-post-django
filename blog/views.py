18f5ea11fcca0c59dd5513ed06d0ee2c447a3e44
mar1fatih <marouanefatih631@gmail.com>
2025-03-13 22:29:43 +0000

    adding template for each post

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
     return render(request, 'blog/post_list.html', { 'posts': posts })
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', { 'post': post })
